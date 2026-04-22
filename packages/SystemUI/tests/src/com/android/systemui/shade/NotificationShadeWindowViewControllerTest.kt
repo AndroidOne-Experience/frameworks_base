98d039a71a3b863c562f1872b0ac7134d13d424b
Arindam BHattacharjee <abhattacharjee717@gmail.com>
2026-04-22 19:12:10 +0000

    Revert "SystemUI: Add double tap to sleep gesture"
    This reverts commit cc21a82b9cd9719b0a98fe23a5343f0ae8f8cfda.

--- packages/SystemUI/tests/src/com/android/systemui/shade/NotificationShadeWindowViewControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/shade/NotificationShadeWindowViewControllerTest.kt
@@ -181,7 +181,6 @@ class NotificationShadeWindowViewControllerTest(flags: FlagsParameterization) : 
     private val notificationLaunchAnimationRepository = NotificationLaunchAnimationRepository()
     private val notificationLaunchAnimationInteractor =
         NotificationLaunchAnimationInteractor(notificationLaunchAnimationRepository)
-    @Mock private lateinit var qqsGestureListener: QQSGestureListener
 
     private val brightnessMirrorShowingRepository = BrightnessMirrorShowingRepository()
     private val brightnessMirrorShowingInteractor =
@@ -278,7 +277,6 @@ class NotificationShadeWindowViewControllerTest(flags: FlagsParameterization) : 
                 kosmos.shadeStatusBarComponentsInteractor,
                 kosmos.dozeTouchInteractor,
                 kosmos.javaAdapter,
-                qqsGestureListener,
             )
         underTest.setupExpandedStatusBar()
         underTest.setDragDownHelper(dragDownHelper)
